d9142c9b6de4027ef5094357e1ca08a19e17832d
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-11-12 10:12:02 +0100

    Olvasd be az országokat

--- LINQ20221112/LINQ20221112/Form1.cs
+++ LINQ20221112/LINQ20221112/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,54 @@ namespace LINQ20221112
 {
     public partial class Form1 : Form
     {
+
+        List<Country> countries = new List<Country>(); 
+
         public Form1()
         {
             InitializeComponent();
+            LoadData("ramen.csv");
+        }
+
+        void LoadData(string fileName)
+        {
+            //országok a 3. oszlop volt
+            //egy ország többször előfordulhat, csak egyszer kell betenni a listába
+
+            StreamReader streamReader = new StreamReader(fileName);
+            streamReader.ReadLine(); //átugrik az első soron
+
+            while (!streamReader.EndOfStream)
+            {
+                string[] sor = streamReader.ReadLine().Split(';'); //split felbontja tömbre ";" jel alapján
+                string country = sor[2];
+                //var eredmeny = countries.Where(i => i.Name.Equals(country)).FirstOrDefault(); //LINQ
+
+                //LINQ SQL-es:
+                var eredmeny = (from c in countries 
+                               where c.Name.Equals(country) 
+                               select c).FirstOrDefault();
+
+                if (eredmeny==null) //nincs ilyen ország a listában, újat létrehozunk
+                {
+                    eredmeny = new Country
+                    {
+                        ID = countries.Count,
+                        Name = country
+                    };
+
+                    countries.Add(eredmeny);
+                }
+            }
+
+            streamReader.Close();
+
+            /*using (resource)
+            {
+
+            }*/
+
+
         }
     }
 }
