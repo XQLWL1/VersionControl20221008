7d48bb99b70c0a4a7f0e7c3cbe48bcfeda5d7995
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-11-13 13:55:16 +0100

    Excel munkafüzet létrehozása és megnyitása

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -17,10 +17,32 @@ namespace ExcelExport
         List<Flat> Flats = new List<Flat>();
         RealEstateEntities context=new RealEstateEntities ();
 
+        Excel.Application xlApp; // Ez az Excel alkalmazás
+        Excel.Workbook xlWB; // létrehoz workbook-ot
+        Excel.Worksheet xlSheet; // sheet-et hoz létre
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            //throw new NotImplementedException();
+
+            xlApp = new Excel.Application (); //új excel létrehozás
+            xlWB = xlApp.Workbooks.Add (); //az excelen belüli workbook és sheet kiválasztása - hova írhatok adatot
+            xlSheet = xlWB.ActiveSheet;
+
+            //CreateTable();
+
+            xlApp.Visible= true; //felhasználó számára elérhetővétesszük az alkalmazást:
+            xlApp.UserControl = true; //felhasználó általi vezérlés engedélyezése
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
