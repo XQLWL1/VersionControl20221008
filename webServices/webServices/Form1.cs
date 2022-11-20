1a8328ad9100630e3b107cefcf7bb8f4a7b81153
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-11-20 13:27:24 +0100

    Webszolgáltatás hívása

--- webServices/webServices/Form1.cs
+++ webServices/webServices/Form1.cs
@@ -15,6 +15,18 @@ namespace webServices
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new webServices.MnbServiceReference.MNBArfolyamServiceSoapClient();
+
+            var request = new webServices.MnbServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate="2020-01-01",
+                endDate="2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
