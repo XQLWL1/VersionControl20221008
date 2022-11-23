ffe9f50d898ab121d30b0d747dec127f9fa98c93
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-11-23 17:26:04 +0100

    XML feldolgozása

--- webServices/webServices/Form1.cs
+++ webServices/webServices/Form1.cs
@@ -7,13 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using webServices.Entities;
 
 namespace webServices
 {
     public partial class Form1 : Form
     {
-        BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<RateData> rates = new BindingList<RateData>();
 
         public Form1()
         {
@@ -31,12 +32,55 @@ namespace webServices
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
 
+
+            XML();
             
+        }
+
+        public void XML(string resultstring)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(resultstring);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                //Adatsor létrehozása:
+                RateData rateData = new RateData();
+
+                //Dátum:
+                rateData.Date = DateTime.Parse(item.GetAttribute("date"));
+
+                //Valuta:
+                XmlElement childElement = (XmlElement)item.ChildNodes[0];
+
+                if (childElement != null)
+                {
+                    rateData.Currency = childElement.GetAttribute("currency");
+
+                    //Az alapegység a gyermek elem "unit" attribútumának segítségével határozható meg
+                    decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+
+                    //az egységhez tartozó érték pedig a gyermek elem InnerText tulajdonságából kapható meg
+                    rateData.Value = decimal.Parse(childElement.InnerText);
+
+                    if (unit!=0)
+                    {
+                        rateData.Value = rateData.Value / unit;
+                    }
+
+                    //hozzáadjuk a listához
+                    rates.Add(rateData);
+                }
+
+
+            }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            dataGridView1.DataSource= Rates;
+            dataGridView1.DataSource= rates;
         }
+
+        
     }
 }
