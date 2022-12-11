52643e49330515f5f7c52a5dc6efad0e06d7ea26
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-12-11 15:25:22 +0100

    portfólió tárolása a memóriában

--- ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
+++ ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ValueAtRisk_VAR.Entities;
 
 namespace ValueAtRisk_VAR
 {
@@ -16,17 +17,38 @@ namespace ValueAtRisk_VAR
         PortfolioEntities context = new PortfolioEntities(); //context példányosítása
         List<Tick> ticks;
 
+        List<PortfolioItem> portfolios = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
 
             ticks =context.Ticks.ToList();
             dataGridView1.DataSource=ticks;
+
+            CreatePortfolio();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void CreatePortfolio()
         {
+            //throw new NotImplementedException();
 
+            PortfolioItem portfolioItem = new PortfolioItem();
+            portfolioItem.Index = "OTP";
+            portfolioItem.Volume = 10;
+            portfolios.Add(portfolioItem);
+
+            PortfolioItem portfolioItem2 = new PortfolioItem();
+            portfolioItem2.Index = "ZWACK";
+            portfolioItem2.Volume = 10;
+            portfolios.Add(portfolioItem2);
+
+            PortfolioItem portfolioItem3 = new PortfolioItem();
+            portfolioItem3.Index = "ELMU";
+            portfolioItem3.Volume = 10;
+            portfolios.Add(portfolioItem2);
+
+            dataGridView2.DataSource= portfolios;
         }
     }
 }
