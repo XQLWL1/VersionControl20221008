a28645932698a3dbe40855c165cc31eded681468
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-12-11 15:10:00 +0100

    object relational mapping, adatok betöltése

--- ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
+++ ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
@@ -12,9 +12,21 @@ namespace ValueAtRisk_VAR
 {
     public partial class Form1 : Form
     {
+
+        PortfolioEntities context = new PortfolioEntities(); //context példányosítása
+        List<Tick> ticks;
+
         public Form1()
         {
             InitializeComponent();
+
+            ticks =context.Ticks.ToList();
+            dataGridView1.DataSource=ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
