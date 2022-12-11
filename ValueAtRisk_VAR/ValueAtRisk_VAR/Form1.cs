7b5a9030aefe8eac7ee10897361da0cf4ccefeea
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2022-12-11 15:36:15 +0100

    Portfólió érték kiszámítása

--- ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
+++ ValueAtRisk_VAR/ValueAtRisk_VAR/Form1.cs
@@ -50,5 +50,24 @@ namespace ValueAtRisk_VAR
 
             dataGridView2.DataSource= portfolios;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+
+            //foreach ciklus végig megy a portfolio elemein (elemek: OTP,ZWACK, ELMU).
+            foreach (var item in portfolios)
+            {
+                var last = (from x in ticks //ticks listában keres
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay //dátumnak kisebbnek vagy =-nek kell lennie a Trading Day-el
+                            select x)
+                            .First(); //az első elem kell
+
+                //az értékhez hozzáadja az utolsó elem értékét és azt megszorozza a Volume-val.
+                value += (decimal)last.Price * item.Volume; 
+            }
+            return value;
+        }
     }
 }
